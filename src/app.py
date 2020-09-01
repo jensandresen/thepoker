9e570c305ec872d388b65204037ff99556a3e726
Jens Andresen <jensandresen@gmail.com>
2020-09-01 12:16:38 +0200

    change to new decentralized service manifests

--- src/app.py
+++ src/app.py
@@ -36,43 +36,79 @@ def load_services_configuration(filename):
         configuration = yaml.safe_load(stream)
         return configuration['services']
 
+def load_service_manifest(source_root):
+    filename = os.path.join(source_root, "pokermanifest.yml")
+    content = read_yaml_file(filename)
+    return content['configuration']
+
 def create_service(service_root, name, properties):
     print('create service ' + name)
 
     print('  building folder structure:')
 
+    # create service root dir
     print('    creating service dir ' + service_root + '...')
     os.mkdir(service_root)
 
-    app_root = os.path.join(service_root, properties['appDir'])
-    print('    cloning repository into app dir "' + app_root + '"...')
-    run('git clone -q ' + properties['repository'] + ' ' + app_root)
+    # clone source into service dir
+    repository_dir = os.path.join(service_root, '_repository')
+    print('    cloning source repository into "' + repository_dir + '"...')
+    run('git clone -q ' + properties['repository'] + ' ' + repository_dir)
 
-    data_root = os.path.join(service_root, properties['dataDir'])
+    # loading service manifest
+    print('    loading service manifest...')
+    service_manifest = None
+    try:
+        service_manifest = load_service_manifest(repository_dir)
+    except FileNotFoundError:
+        print('error! no service manifest was found!')
+        return
+
+    # create data dir
+    data_root = os.path.join(service_root, service_manifest['directories']['data'])
     print('    creating data dir "' + data_root + '"...')
     os.mkdir(data_root)
 
+    # executing commands
+    os.chdir(repository_dir)
     print('  executing commands:')
+    print('    cwd: ' + repository_dir)
 
-    setup_command = properties['setupCommand']
-    if setup_command:
-        print('    setup ("' + setup_command + '")...')
-        run(setup_command)
+    env_vars = build_env_vars_from(service_root, service_manifest)
+    run_command(service_manifest, 'setup', env_vars, '    ')
+    run_command(service_manifest, 'run', env_vars, '    ')
 
-    run_command = properties['runCommand']
-    if run_command:
-        print('    run ("' + run_command + '")...')
-        run(run_command)
+def build_env_vars_from(service_root, service_manifest):
+    return {
+        "DATA_DIR": os.path.join(service_root, service_manifest['directories']['data'])
+    }
+
+def run_command(service_manifest, command_name, env_vars, indentation):
+    local_indentation = indentation
+    print(local_indentation + command_name)
+
+    local_indentation += '  ';
+
+    cmd = service_manifest['commands'][command_name]
+    if cmd:
+        # print and join env vars
+        print(local_indentation + 'env:')
+        env_vars_full = ''
+        for key, value in env_vars.items():
+            new_entry = key + '=' + value
+            print(local_indentation + '  ' + new_entry)
+            env_vars_full += new_entry + ' '
+
+        print(local_indentation + 'cmd: ' + cmd)
+        run(env_vars_full + cmd)
     else:
-        print('error! no "run command" has been specified for service "' + name + '"!')
+        print(local_indentation + 'no command "' + command_name + '" was specified')
 
 def update_service(service_root, name, properties):
     print('update service ' + name)
 
-    app_root = os.path.join(service_root, properties['appDir'])
-    data_root = os.path.join(service_root, properties['dataDir'])
-
-    os.chdir(app_root)
+    repository_dir = os.path.join(service_root, '_repository')
+    os.chdir(repository_dir)
 
     # download repository changes
     print('  fetching repository origin...')
@@ -84,24 +120,51 @@ def update_service(service_root, name, properties):
     if has_changes:
         print('  changes detected')
 
-        # run teardown        
-        teardown_command = properties['teardownCommand']
-        print('    executing teardown command ("' + teardown_command + '")...')
-        run(teardown_command)
+        # load current/old service manifest
+        service_manifest = None
+        try:
+            service_manifest = load_service_manifest(repository_dir)
+        except FileNotFoundError:
+            print('error! no service manifest was found!')
+            return
+
+        # first, teardown
+        print('  executing teardown:')
+        print('    cwd: ' + repository_dir)
+        env_vars = build_env_vars_from(service_root, service_manifest)
+        run_command(service_manifest, 'teardown', env_vars, '    ')
 
         # update repository
         print('  update repository...')
         run('git merge -q --ff origin/master')
 
+        # load NEW service manifest
+        try:
+            service_manifest = load_service_manifest(repository_dir)
+        except FileNotFoundError:
+            print('error! no service manifest was found!')
+            return
+            
+        env_vars = build_env_vars_from(service_root, service_manifest)
+
+        # run setup
+        print('  executing setup:')
+        print('    cwd: ' + repository_dir)
+        run_command(service_manifest, 'setup', env_vars, '    ')
+
         # run the run command
-        run_command = properties['runCommand']
-        print('    executing run command ("' + run_command + '")...')
-        run(run_command)
+        print('  executing run:')
+        print('    cwd: ' + repository_dir)
+        run_command(service_manifest, 'run', env_vars, '    ')
     else:
         print('  no changes detected')
 
 def run(command):
     with os.popen(command) as stream:
         return stream.read()
+
+def read_yaml_file(filename):
+    with open(filename, 'r') as stream:
+        return yaml.safe_load(stream)
 
 if __name__ == '__main__': main()
